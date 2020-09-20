d4b85ab502c91938971cb1b666fb7341705fd683
RomarioBispo <romario.bsp.silva@gmail.com>
2020-09-20 10:42:21 -0300

    project: configuração do modulo de api

---
+++ desafio-api/src/main/java/br/com/tinnova/desafio/api/DesafioApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.tinnova.desafio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApiApplication.class, args);
+	}
+
+}
